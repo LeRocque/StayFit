37e173932e16dae4ebc968e29d5a5a19fdeb7ed5
LeRocque <t.lerocque@gmail.com>
2023-07-06 09:53:54 -0400

    redux setup complete

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
     port: 5000,
     proxy: {
       "/user": "http://localhost:3000/",
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "/test/setup.ts",
+    mockReset: true,
   },
 });
