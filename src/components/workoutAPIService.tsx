594bda83dfaeb434e7c863341816f5ad4f3001db
LeRocque <t.lerocque@gmail.com>
2023-08-30 14:26:06 -0400

    moved tables to model

---
+++ src/components/workoutAPIService.tsx
@@ -0,0 +1,16 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// export const workoutApi = createApi({
+//   reducerPath: "workoutApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://wger.de/api/v2/",
+//   }),
+//   endpoints: (builder) => ({
+//     getImages: builder.query({
+//       query: (options) => `exerciseimage/?${options}`,
+//       transformResponse: (response) => response.results,
+//     }),
+//   }),
+// });
+
+// export const { useGetImagesQuery } = workoutApi;
