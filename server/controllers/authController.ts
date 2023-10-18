bbff0945cd5ca7d8e35ca8865ed13b67fa892c43
LeRocque <t.lerocque@gmail.com>
2023-10-18 16:03:12 -0400

    commented authController.ts

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -3,11 +3,15 @@ import jwt from "jsonwebtoken";
 import * as jwtStrat from "jose";
 import { CookieVals, ResLocalsAssignJwt } from "../backendTypes";
 
+// initialize to env variable or empty string if not provided
 const jwtSecret = process.env.JWT_SECRET || "";
+// encode secret for JWT varification
 const secretEncoded = new TextEncoder().encode(jwtSecret);
+// set age for 3 days (days, hours, minutes, seconds)
 const maxAge = 3 * 24 * 60 * 60;
 
 const authController = {
+  // method that will create a JWT with the user.id persisted in res.locals and our secret
   assignJwt: (_req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = res.locals as ResLocalsAssignJwt;
@@ -26,6 +30,7 @@ const authController = {
     }
   },
 
+  // method that will create cookies for user token and id
   cookieCreator: (_req: Request, res: Response, next: NextFunction) => {
     try {
       const { token } = res.locals;
@@ -50,6 +55,7 @@ const authController = {
     }
   },
 
+  // method that will verify that a user has a valid JWT, as well as verify that the JWT belongs to that user (keep the user from accessing other user workout pages)
   isAuthenticated: async (req: Request, _res: Response, next: NextFunction) => {
     const { token } = req.cookies as CookieVals;
     const { ssid } = req.cookies as CookieVals;
